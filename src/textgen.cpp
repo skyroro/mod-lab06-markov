da444e6492b17f9384a37dca1f09a6ba9ab4b5d6
Pachurina Darya <126852495+skyroro@users.noreply.github.com>
2023-04-30 21:29:29 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -39,7 +39,7 @@ std::string generateText(int NPREF, int MAXGEN,
         }
         int n = suf.size();
         srand(time(NULL));
-        int r = rand() % n;
+        int r = std::rand() % n;
         std::string word = suf[r];
         count += word.size();
         text = text + word + " ";
