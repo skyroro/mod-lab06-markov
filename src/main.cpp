5321d86d53ab697bb5355c1a15d62d641f57b0fd
Pachurina Darya <126852495+skyroro@users.noreply.github.com>
2023-04-30 20:13:48 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+#include "textgen.h"  
+
+int main() {
+    const int NPREF = 2;
+    const int MAXGEN = 10;
+    std::string filename = "input.txt";
+    //вызвать создание таблицы
+    typedef std::deque<std::string> prefix;
+    std::map<prefix, std::vector<std::string>> statetab = generateMarkov(NPREF, filename);
+    //создаем начальный префикс
+    std::vector<std::string> start_words;
+    std::ifstream file(filename);
+    if (!file.is_open()) std::cout << "no";
+    int word_count = 0;
+    std::string word;
+    while (file >> word) {
+        start_words.push_back(word);
+    }
+    file.close();
+    prefix intitalPref;
+    srand(time(NULL));
+    int index = rand() % start_words.size();
+    for (int i = 0; i < NPREF; i++) {
+        intitalPref.push_back(start_words[index + i]);
+    }  
+    //генерируем текст
+    std::string text = generateText(NPREF, MAXGEN, intitalPref, statetab);
+    std::cout << text;
+    return 0;
+}
